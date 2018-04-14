8a6173bdd832b34ef90f32a2bcb3ba52b217ad3f
rohan23chhabra <rohan23chhabra@gmail.com>
2018-04-14 10:48:12 +0530

    Everything done and dusted

--- popup.js
+++ popup.js
@@ -15,5 +15,20 @@ function pasteSelection() {
 }
 
 function setDetails(word) {
-
+    let xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            let res = xhr.responseText;
+            console.log(res);
+            let obj = JSON.parse(res);
+            document.getElementById('bold-word').innerHTML = word.toUpperCase();
+            document.getElementById('definition').innerHTML = obj.results[0].lexicalEntries[0].entries[0].senses[0].definitions[0];
+            document.getElementById('etymology').innerHTML = obj.results[0].lexicalEntries[0].entries[0].etymologies[0];
+            document.getElementById('part-of-speech').innerHTML = obj.results[0].lexicalEntries[0].lexicalCategory;
+            document.getElementById('language').innerHTML = obj.results[0].lexicalEntries[0].language;
+        }
+    };
+    let url = 'http://localhost:8000/wordy?word=' + word;
+    xhr.open('GET', url, true);
+    xhr.send();
 }
--- server/myserver/wordy/views.py
+++ server/myserver/wordy/views.py
@@ -1,8 +1,18 @@
-from django.shortcuts import render
-
 # Create your views here.
 
-from django.http import HttpResponse
+import requests
+from django.http import JsonResponse
+
 
 def index(request):
-	return HttpResponse("Hello world!, You are at the wordy index")
+    word = request.GET.get('word')
+    app_id = '39e44a1d'
+    app_key = '7659768a10f8611c001df1b4a054be9e'
+    language = 'en'
+    proxies = {
+        'http': 'http://edcguest:edcguest@172.31.102.29:3128',
+        'https': 'https://edcguest:edcguest@172.31.102.29:3128'
+    }
+    url = 'https://od-api.oxforddictionaries.com:443/api/v1/entries/' + language + '/' + word.lower()
+    r = requests.get(url, headers={'app_id': app_id, 'app_key': app_key}, proxies=proxies)
+    return JsonResponse(r.json())
