94ac059f431e6cfa416abe815209e4dabc747394
rohan23chhabra <rohan23chhabra@gmail.com>
2018-04-14 10:40:13 +0530

    Final commit

--- xml2json.js
+++
@@ -1,8 +0,0 @@
-/*	This work is licensed under Creative Commons GNU LGPL License.
-
-	License: http://creativecommons.org/licenses/LGPL/2.1/
-   Version: 0.9
-	Author:  Stefan Goessner/2006
-	Web:     http://goessner.net/ 
-*/
-
